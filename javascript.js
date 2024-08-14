e3a2ade94b5a7148a61c41bb0359ecc9aacdc8d0
Sh3dow-ware <u2486896@rts-365.dk>
2024-08-14 13:40:52 +0200

    ​

--- javascript.js
+++
@@ -1,9 +0,0 @@
-const observer = new PerformanceObserver((list) => {
-    list.getEntries().forEach((entry) => {
-      if (entry.identifier === "big-image") {
-        console.log(entry.loadTime);
-      }
-    });
-  });
-  observer.observe({ type: "element", buffered: true });
-  
